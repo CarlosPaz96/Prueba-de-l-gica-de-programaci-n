3b63016e38ff7f5940579e411f4b95fc28e04aad
Carlos Vasquez <38087007+CarlosPaz96@users.noreply.github.com>
2022-01-21 14:06:47 -0600

    Update PruebaLogicaProgramacion.js
    Agregando validaciones: se valida que los datos ingresados sean números y mayores a 0, se valida que los precios ingresados sean mayores al anterior, se da la opción de eliminar los registros y empezar de nuevo o de continuar desde donde se comete el error

--- PruebaLogicaProgramacion.js
+++ PruebaLogicaProgramacion.js
@@ -1,36 +1,101 @@
 var I;//numero de millas que desea viajar
-var tarifa=[];//Lista de precios
+var tarifa = [];//Lista de precios
 const coche = ["UberX", "UberXL", "Uber Plus", "Uber Black", "Uber SUV"];//lista de carros
-var cont = 0;//contador para el while
+var cont = 0;//contador para el while de tarifas
+var Icont = 0;//contador para el while de millas
+var n = 0;//variable que almacena el ingreso antes de validar
 
-console.log("Por ser un nuevo usuario de Uber, tienes 20 dólares de descuento en tu primer viaje.")
-I = prompt('Ingrese el numero de millas que desea viajar:');
-console.log('Numero de millas que desea viajar:' + I);
-console.log('Ingrese los 5 precios de las milla desde el mas barato al mas caro');
-while (cont<5) {
-    let cont2 = cont+1;//contador para los mensajes
-    let pA= cont-1; //contador para la posicion anterior
-    let n = 0;//variable que almacena el ingreso antes de validar
-    if (tarifa.length==0) {
-        tarifa[cont] = prompt('Ingrese el precio numero '+ cont2);
-        console.log('El '+cont2+' º precio ingresado es '+ tarifa[cont]);
-        cont = cont+1;
+
+document.write("Por ser un nuevo usuario de Uber, tienes 20 dólares de descuento en tu primer viaje." + "<br>")
+
+while (Icont < 1) {
+    n = prompt('Ingrese el numero de millas que desea viajar:');
+    if (!isNaN(n)) {
+        if (n > 0) {
+            I = n;
+            ++Icont;
+        } else {
+            document.write('----------------------------------------------' + "<br>")
+            document.write('El numero de millas debe ser mayor a Cero' + "<br>")
+            document.write('----------------------------------------------' + "<br>")
+        }
     } else {
-        n = prompt('Ingrese el precio numero '+ cont2);
-        tarifa[cont]=n;
-        console.log('El '+cont2+' º precio ingresado es '+ tarifa[cont]);
-        cont = cont+1;
+        document.write('----------------------------------------------' + "<br>")
+        document.write('Debe ingresar valores numericos mayores a cero' + "<br>")
+        document.write('----------------------------------------------' + "<br>")
     }
 }
 
-//console.log('sali del while');
+document.write('Numero de millas que desea viajar:' + I + "<br>");
 
-/*for (let i = 0; i < tarifa.length; i++) {
-    console.log('A- '+tarifa[i])
-    
-}*/
+document.write('Ingrese los 5 precios de las milla desde el mas barato al mas caro' + "<br>");
+while (cont < 5) {
+    let cont2 = cont + 1;//contador para los mensajes
+    let pA = cont - 1; //contador para la posicion anterior
+    n = prompt('Ingrese el precio numero ' + cont2 + ' Para el servicio ' + coche[cont]);
+    if (!isNaN(n)) {//isNaN retorna false para los numeros por eso se lleva el !
+        if (tarifa.length == 0) {
+            //console.log('El '+cont2+' º precio ingresado es '+ n);
+            if (n > 0) {
+                let servicio = {
+                    coche: coche[cont],
+                    precio: n
+                };
+                tarifa[cont] = servicio;
+                document.write('------>   Guardado Exitosamente ' + tarifa[cont].coche + ' : $' + tarifa[cont].precio + ' por milla ' + "<br>")
+                cont = cont + 1;
+                n = 0;
+            }
+            else {
+                document.write('----------------------------------------------' + "<br>")
+                document.write('Debe ingresar valores numericos mayores a cero' + "<br>")
+                document.write('----------------------------------------------' + "<br>")
+            }
+        } else {
+            if (n > tarifa[pA].precio) {
+                let servicio = {
+                    coche: coche[cont],
+                    precio: n
+                };
+                tarifa[cont] = servicio;
+                document.write('------>   Guardado Exitosamente ' + tarifa[cont].coche + ' : $' + tarifa[cont].precio + ' por milla '+"<br>")
+                cont = cont + 1;
+                n = 0;
+            } else {
+                n = 0;//reiniciando la variable 'n' para evitar errores en la validacion
+                document.write('----------------------------------------------' + "<br>")
+                document.write('El precio ingresado es menor al anterior, intentelo de nuevo o pulse 0 (cero) para ingresar los precios desde el inicio o cualquier otra tecla para continuar' + "<br>")
+                document.write('----------------------------------------------' + "<br>")
+                let opcion = prompt('Pulse 0 (cero) para ingresar todo desde el inicio o cualquier otra letra para continuar')
+                if (opcion == 0) {
+                    tarifa = [];
+                    cont = 0;
+                }
+            }
+
+        }
+    } else {
+        document.write('----------------------------------------------' + "<br>")
+        document.write('El dato ingresado no es valido, intente de nuevo' + "<br>");
+        document.write('----------------------------------------------' + "<br>")
+    }
+}
+
+document.write('Calculando el mejor Carro en el que puede viajar....' + "<br>")
+
+
+
+
+fancyRide(I, tarifa);
 
 
 function fancyRide(l, tarifas) {
+    let Viaje = [];
+    for (let ii = 0; ii < tarifas.length; ii++) {
+        let num = l * tarifas[ii];
+        if (num < 20) {
+            Viaje[ii] = num;
+        }
 
+    }
 }
